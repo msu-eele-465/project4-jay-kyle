beb4239740e5c95071016965a4591bc103313c04
Jay <jaybirdinrlmt@gmail.com>
2025-02-28 00:24:18 -0700

    initial development of keypad functionality for master device. Switching from a polling approach to an ISR approach for better efficiency.

--- controller/app/main.c
+++ controller/app/main.c
@@ -66,8 +66,39 @@
 //******************************************************************************
 #include <msp430.h>
 
+char pressed_key = 'N';                     // holds key that was pressed
+int password_index = 1;                     // tracks which digit is being entered
+int status = 0;                             // tracks locked, unlocked, or unlocking
+int locked = 0;                             // status value for locked
+int unlocking = 1;                          // status value for unlocking
+int unlocked = 2;                           // status value for unlocked
+char password_char1 = '5';                  // first digit of password
+char password_char2 = '2';                  // second digit of password
+char password_char3 = '9';                  // third digit of password
+char password_char4 = '3';                  // fourth digit of password
+
+void keypad_init(void) {
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer           
+
+    P1DIR |= (BIT4 | BIT5 | BIT6 | BIT7);   // Set keypad row pins as outputs
+    P1OUT |= (BIT4 | BIT5 | BIT6 | BIT7);   // sets output high to start
+
+    P2DIR &= ~(BIT0 | BIT1 | BIT2 | BIT3);  // Set P1.4 - p1.7 as input
+    P2REN |= (BIT0 | BIT1 | BIT2 | BIT3);   // Enable pull-up/down resistors
+    P2OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);  // Set as pull-down
+    P2IES |= (BIT0 | BIT1 | BIT2 | BIT3);   // Config IRQ sensitivity H-to-L
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable low power mode
+
+    P2IFG &= ~(BIT0 | BIT1 | BIT2 | BIT3);  // Clear P2.0-2.3 IRQ Flag
+    P2IE |= (BIT0 | BIT1 | BIT2 | BIT3);    // Enable P2.0-2.3 IRQ
+    __enable_interrupt();                   // Enable Maskable IRQs
+}
+
 int main(void)
 {
+    keypad_init();
+
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
     
     P1OUT &= ~BIT0;                         // Clear P1.0 output latch for a defined power-on state
@@ -81,4 +112,77 @@ int main(void)
         P1OUT ^= BIT0;                      // Toggle P1.0 using exclusive-OR
         __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
     }
+}
+
+void get_key(void) {
+    char key_map[4][4] = {
+        {'1', '2', '3', 'A'},
+        {'4', '5', '6', 'B'},
+        {'7', '8', '9', 'C'},
+        {'*', '0', '#', 'D'}
+    };
+
+    for (int row = 0; row < 4; row++) {
+        P1OUT &= ~(BIT4 | BIT5 | BIT6 | BIT7);  // Clear all rows
+        P1OUT |= (BIT4 << row);  // Set one row high at a time
+        __delay_cycles(1000); 
+
+        for (int col = 0; col < 4; col++) {
+            if (P2IN & (BIT0 << col)) {  // Check if column is high
+                process_key(key_map[row][col]);
+            }
+        }
+    }
+    P1OUT |= (BIT4 | BIT5 | BIT6 | BIT7);  // Reset all rows
+    return 'N';  // No key pressed
+}
+
+void process_key(char pressed_key) {
+    if (status == locked || status == unlocking) {
+        check_password(pressed_key);
+    } else {
+        // send data to slave with I2C
+    }
+}
+
+void check_password(char pressed_key) {
+    if (password_index == 1) {
+        if (pressed_key == password_char1) {
+            status = unlocking;
+            password_index = 2;
+        } else {
+            status = locked;
+            password_index = 1;
+        }
+    } else if (password_index == 2) {
+        if (pressed_key == password_char2) {
+            status = unlocking;
+            password_index = 3;
+        } else {
+            status = locked;
+            password_index = 1;
+        }
+    } else if (password_index == 3) {
+        if (pressed_key == password_char3) {
+            status = unlocking;
+            password_index = 4;
+        } else {
+            status = locked;
+            password_index = 1;
+        }
+    } else if (password_index == 4) {
+        if (pressed_key == password_char4) {
+            status = unlocked;
+            password_index = 1;
+        } else {
+            status = locked;
+            password_index = 1;
+        }
+    }
+}
+
+#pragma vector=PORT2_VECTOR
+__interrupt void KEYPAD_ISR(void) {
+    get_key();
+    P2IFG &= ~(BIT0 | BIT1 | BIT2 | BIT3);
 }
