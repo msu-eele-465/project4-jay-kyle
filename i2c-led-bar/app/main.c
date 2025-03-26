9e2efd1505318a2fe98b26cef5cc5d2c4f2ab359
Jay <jaybirdinrlmt@gmail.com>
2025-03-25 22:12:49 -0600

    full outline of led bar controller with pattern control adapted from project 3 and some slightly different init handling. Still need to iron out i2c reading

--- i2c-led-bar/app/main.c
+++ i2c-led-bar/app/main.c
@@ -66,6 +66,73 @@
 //******************************************************************************
 #include <msp430.h>
 
+int status = 0;         // status of the keypad
+int unlocked = 2;       
+int unlocking = 1;
+int locked = 0;
+
+int pattern = 0;     
+int current_pattern = 0;   
+int next_pattern = 0;    
+int pattern1 = 0;                       
+int pattern2 = 0;                           
+int pattern3 = 0;
+int pattern4 = 0;
+int pattern5 = 0;
+int pattern6 = 0;
+int pattern7 = 0;
+int pattern3_step = 0;
+int pattern6_step = 0;
+
+float base_transition_scalar = 1.0;   
+
+void init_led_bar(void) {
+    WDTCTL = WDTPW | WDTHOLD;                    // Stop watchdog timer           
+    PM5CTL0 &= ~LOCKLPM5;                        // Disable High Z mode
+
+    // Set P1.0-1.1, P1.4-1.7, P2.6-2.7 as outputs for led bar
+    P1DIR |= (BIT0 | BIT1 | BIT4 | BIT5 | BIT6 | BIT7);
+    P1OUT |= (BIT0 | BIT1 | BIT4 | BIT5 | BIT6 | BIT7);
+    P2DIR |= (BIT6 | BIT7);
+    P2OUT |= (BIT6 | BIT7);
+
+    // Configure Timer B0
+    TB0CTL |= (TBSSEL__ACLK | MC__UP | TBCLR); // Use ACLK, up mode, clear
+    TB0CCR0 = (int)(base_transition_scalar * 32768);
+
+    // Enable and clear interrupts for each color channel
+    TB0CCTL0 |= CCIE;                            // Interrupt for pattern transistions
+    TB0CCTL0 &= ~CCIFG;
+
+    __enable_interrupt();                        // enable interrupts
+}
+
+void i2c_b0_init(void) {
+    WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
+
+    UCB0CTLW0 |= UCSWRST;                   // Put eUSCI_B0 in SW Reset
+    UCB0CTLW0 |= UCSSEL__SMCLK;             // Choose BRCLK = SMCLK = 1Mhz
+    UCB0BRW = 10;                           // Divide BRCLK by 10 for SCL = 100kHz
+    UCB0CTLW0 |= UCMODE_3;                  // Put into I2C mode
+    UCB0CTLW0 &= ~UCMST;                    // Put into slave mode
+    UCB0CTLW0 |= UCTR;                      // Put into Tx mode
+    UCB0I2CSA = 0x0020;                     // Slave address = 0x20
+    UCB0CTLW1 |= UCASTP_2;                  // Auto STOP when UCB0TBCNT reached
+    UCB0TBCNT = sizeof(LCD_Packet);         // # of bytes in packet
+
+    P1SEL1 &= ~BIT3;                        // P1.3 = SCL
+    P1SEL0 |= BIT3;                            
+    P1SEL1 &= ~BIT2;                        // P1.2 = SDA
+    P1SEL0 |= BIT2;
+
+    PM5CTL0 &= ~LOCKLPM5;                   // Disable low power mode
+
+    UCB0CTLW0 &= ~UCSWRST;                  // Take eUSCI_B0 out of SW Reset
+
+    UCB0IE |= UCTXIE0;                      // Enable I2C Tx0 IR1
+    __enable_interrupt();                   // Enable Maskable IRQs
+}
+
 int main(void)
 {
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
@@ -80,5 +147,157 @@ int main(void)
     {
         P1OUT ^= BIT0;                      // Toggle P1.0 using exclusive-OR
         __delay_cycles(100000);             // Delay for 100000*(1/MCLK)=0.1s
+    }
+}
+
+void process_i2c_data(void) {
+    status = 0;
+    pattern = 0;
+    base_transition_scalar = 0;
+    update_led_bar(status, pattern, base_transition_scalar);
+}
+
+void write_8bit_value(int byte) {
+    P1OUT &= ~(BIT0 | BIT1 | BIT4 | BIT5 | BIT6 | BIT7);
+    P2OUT &= ~(BIT6 | BIT7);
+
+    if (byte & (1 << 0)) P1OUT |= BIT7;  // bit 0 → P1.7
+    if (byte & (1 << 1)) P1OUT |= BIT6;  // bit 1 → P1.6
+    if (byte & (1 << 2)) P1OUT |= BIT5;  // bit 2 → P1.5
+    if (byte & (1 << 3)) P1OUT |= BIT4;  // bit 3 → P1.4
+    if (byte & (1 << 4)) P2OUT |= BIT7;  // bit 4 → P2.7
+    if (byte & (1 << 5)) P2OUT |= BIT6;  // bit 5 → P2.6
+    if (byte & (1 << 6)) P1OUT |= BIT1;  // bit 6 → P1.1
+    if (byte & (1 << 7)) P1OUT |= BIT0;  // bit 7 → P1.0
+}
+
+void update_led_bar(int status, int pattern, int base_transition_scalar) {
+    if (status == unlocked) {                   
+        next_pattern = pattern;
+        if (next_pattern == current_pattern) {
+            if (pattern == 1) {
+                pattern1 = 0b01010101;
+            } else if (pattern == 2) {
+                pattern2 = 0b11111111;
+            } else if (pattern == 3) {
+                pattern3_step = 0;
+            } else if (pattern == 4) {
+                pattern4 = 0b00000000;      
+            } else if (pattern == 5) {
+                pattern5 = 0b10000000;
+            } else if (pattern == 6) {
+                pattern6_step = 0;
+            } else if (pattern == 7) {
+                pattern7 = 0b11111111;
+            }
+        }
+        current_pattern = next_pattern;
+    }
+    TB0CCR0 = (int)(base_transition_scalar * 8192);
+}
+
+#pragma vector = TIMER0_B0_VECTOR
+__interrupt void Pattern_Transition_ISR(void) {
+    if (status == unlocked) {
+        if (pattern == 0) {
+            write_8bit_value(0b10101010);
+        } else if (pattern == 1) {
+            if (pattern1 == 0b01010101) { 
+                pattern1 = 0b10101010;       
+            } else {
+                pattern1 = 0b01010101;
+            }
+            write_8bit_value(pattern1);
+        } else if (pattern == 2) {
+            if (pattern2 != 0b11111111) {
+                pattern2 = pattern2 + 1;
+            } else {
+                pattern2 = 0b00000000;
+            }
+            write_8bit_value(pattern2);
+        } else if (pattern == 3) {
+            if (pattern3_step == 0) {
+                pattern3 = 0b00011000;
+                pattern3_step = 1;
+            } else if (pattern3_step == 1) {
+                pattern3 = 0b00100100;
+                pattern3_step = 2;
+            } else if (pattern3_step == 2) {
+                pattern3 = 0b01000010;
+                pattern3_step = 3;
+            } else if (pattern3_step == 3) {
+                pattern3 = 0b10000001;
+                pattern3_step = 4;
+            } else if (pattern3_step == 4) {
+                pattern3 = 0b01000010;
+                pattern3_step = 5;
+            } else if (pattern3_step == 5) {
+                pattern3 = 0b00100100;
+                pattern3_step = 0;
+            }
+            write_8bit_value(pattern3);
+        } else if (pattern == 4) {
+            if (pattern4 == 0b00000000) {
+                pattern4 = 0b11111111;
+            } else {
+                pattern4 = pattern4 - 1;
+            }
+            write_8bit_value(pattern4);
+        } else if (pattern == 5) {
+            if (pattern5 == 0 || pattern5 == 0b10000000) {
+                pattern5 = 0b00000001;
+            } else {
+                pattern5 = pattern5 * 2;       
+            } 
+            write_8bit_value(pattern5);
+        } else if (pattern == 6) {
+            if (pattern6_step == 0) {
+                pattern6 = 0b01111111;
+                pattern6_step = 1;
+            } else if (pattern6_step == 1) {
+                pattern6 = 0b10111111;
+                pattern6_step = 2;
+            } else if (pattern6_step == 2) {
+                pattern6 = 0b11011111;
+                pattern6_step = 3;
+            } else if (pattern6_step == 3) {
+                pattern6 = 0b11101111;
+                pattern6_step = 4;
+            } else if (pattern6_step == 4) {
+                pattern6 = 0b11110111;
+                pattern6_step = 5;
+            } else if (pattern6_step == 5) {
+                pattern6 = 0b11111011;
+                pattern6_step = 6;
+            } else if (pattern6_step == 6) {
+                pattern6 = 0b11111101;
+                pattern6_step = 7;
+            } else if (pattern6_step == 7) {
+                pattern6 = 0b11111110;
+                pattern6_step = 0;
+            }
+            write_8bit_value(pattern6);
+        } else if (pattern == 7) {
+            if (pattern7 != 0b11111111) {
+                pattern7 = pattern7 * 2 + 1;       
+            } else {
+                pattern7 = 0b00000001;
+            }
+            write_8bit_value(pattern7);
+        }
+    } else {
+        write_8bit_value(0b00000000);
+    }
+    TB0CCTL0 &= ~ CCIFG;            // Clear interrupt flag
+}
+
+#pragma vector=EUSCI_B0_VECTOR
+__interrupt void LCD_I2C_ISR(void){
+    if (LCD_Data_Cnt == (sizeof(LCD_Packet) - 1)) {
+        UCB0TXBUF = LCD_Packet[LCD_Data_Cnt];
+        LCD_Data_Cnt = 0;
+    } else {
+        UCB0TXBUF = LCD_Packet[LCD_Data_Cnt];
+        LCD_Data_Cnt++;
     }
 }
